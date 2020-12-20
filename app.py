eaf7469358205d6e067f870fdf896e12e1fcc5c5
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-20 16:13:07 -0500

    Initial Flask app code

---
+++ app.py
@@ -0,0 +1,17 @@
+# Import in Dependencies
+from flask import Flask, render_template
+
+# Creating new application of the flask module
+app = Flask(__name__)
+
+# Creating the test route
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Spin-up the Flask Application
+if __name__ == '__main__':
+    app.run(debug=True)
