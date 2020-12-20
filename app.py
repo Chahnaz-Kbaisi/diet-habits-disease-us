157bbac3348a82818d395788d491da8677cfdc4d
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-20 16:44:27 -0500

    Code Updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name="Group 1")
 
 
 # Spin-up the Flask Application
