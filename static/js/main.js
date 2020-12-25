70d79e872d2edf1c3886051eff221e6125570840
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-24 21:40:50 -0500

    main'js file is now ready for new code

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,3 @@
 // console.log('hi');
-d3.json("/tasks").then((tasks) => {
-    // console.log(tasks);
-    var list = d3.select("#tasks");
 
-
-    tasks.forEach((task) => {
-        var item = list.append("li");
-        item.classed("list-group-item", true);
-        item.text(task.description);
-    });
-
-
-});
 
