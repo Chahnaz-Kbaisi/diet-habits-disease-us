35a7855d947ec59bdfe6b526afcd9c88bce53510
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-30 01:41:43 -0500

    Initial commit

---
+++ static/js/lineplot.js
@@ -0,0 +1,174 @@
+// Change page title
+d3.select("#page-title").text("Interactive Visualizations");
+
+// Array variable to hold table data 
+var tableData = []
+
+/***************************************************
+USER DEFINED FUNCTIONS
+****************************************************/
+
+// function creates County Level Line Plot
+function createCountyLevelPlot(data, state, county, impact) {
+    var yearFilter = data.filter(row => row["Year"] != "2010");
+    var stateFilter = yearFilter.filter(row => row["State"] === state);
+    var countyFilter = stateFilter.filter(row => row["County"] == county);
+
+    var yearArray = countyFilter.map(row => row["Year"]);
+    var impactArray = countyFilter.map(row => row[impact]);
+    var obesityArray = countyFilter.map(row => row["% Adults with Obesity"]);
+    var diabetesArray = countyFilter.map(row => row["% Adults with Diabetes"]);
+
+    if (impact == "Median Household Income") {
+        impactArray = impactArray.map(impactVal => impactVal / 1000)
+        impact = impact + " (in Thousands)"
+    }
+    var impactTrace = {
+        x: yearArray,
+        y: impactArray,
+        name: impact,
+        mode: 'lines+markers',
+    };
+
+    var obesityTrace = {
+        x: yearArray,
+        y: obesityArray,
+        name: '% Adults with Obesity',
+        mode: 'lines+markers',
+    };
+
+    var diabetesTrace = {
+        x: yearArray,
+        y: diabetesArray,
+        name: '% Adults with Diabetes',
+        mode: 'lines+markers',
+    };
+
+    var dataCountyLevelPlot = [impactTrace, obesityTrace, diabetesTrace];
+
+    var layoutCountyLevelPlot = {
+        title: `${impact} vs Disease Prevalence - ${county},${state}`
+    };
+
+    Plotly.newPlot('countyLevelPlot', dataCountyLevelPlot, layoutCountyLevelPlot);
+}
+
+// function creates State Level Line Plot
+function createStateLevelPlot(data, state, impact) {
+    var yearFilter = data.filter(row => row["Year"] != "2010");
+    var stateFilter = yearFilter.filter(row => row["State"] === state);
+    var countyFilter = stateFilter.filter(row => row["County"] === "");
+
+    var yearArray = countyFilter.map(row => row["Year"]);
+    var impactArray = countyFilter.map(row => row[impact]);
+    var obesityArray = countyFilter.map(row => row["% Adults with Obesity"]);
+    var diabetesArray = countyFilter.map(row => row["% Adults with Diabetes"]);
+
+    if (impact == "Median Household Income") {
+        impactArray = impactArray.map(impactVal => impactVal / 1000)
+        var impact = impact + " (in Thousands)"
+    }
+
+    var impactTrace = {
+        x: yearArray,
+        y: impactArray,
+        name: impact,
+        mode: 'lines+markers',
+    };
+
+    var obesityTrace = {
+        x: yearArray,
+        y: obesityArray,
+        name: '% Adults with Obesity',
+        mode: 'lines+markers',
+    };
+
+    var diabetesTrace = {
+        x: yearArray,
+        y: diabetesArray,
+        name: '% Adults with Diabetes',
+        mode: 'lines+markers',
+    };
+    var dataStateLevelPlot = [impactTrace, obesityTrace, diabetesTrace];
+
+    var layoutStateLevelPlot = {
+        title: `${impact} vs Disease Prevalence - ${state}`
+    };
+
+    Plotly.newPlot('stateLevelPlot', dataStateLevelPlot, layoutStateLevelPlot);
+}
+
+// function loads County Dropdown options
+function loadCountyDropDown(selectedState) {
+    var stateFilteredData = tableData.filter(row => row.State === selectedState);
+    var countiesList = stateFilteredData.map(row => row.County);
+    var uniqueCounties = d3.set(countiesList).values();
+
+    // sort the counties in ascending
+    uniqueCounties.sort(d3.ascending)
+
+    // Load the County dropdown
+    var countyDropDown = d3.select("#county-select");
+    countyDropDown.html("");
+    uniqueCounties.forEach(county => {
+        if (county != "") {
+            var cell = countyDropDown.append("option");
+            cell.property("value", county).text(county);
+        }
+    });
+}
+
+/***************************************************
+EVENT HANDLERS
+****************************************************/
+
+// State Event Handler - Load County dropdown and State/County Level Plots
+function stateChanged(selectedState) {
+
+    // Load County dropdown
+    loadCountyDropDown(selectedState);
+
+    var county = d3.select("#county-select").property("value");
+    var impact = d3.select("#impact-select").property("value");
+
+    createStateLevelPlot(tableData, selectedState, impact);
+    createCountyLevelPlot(tableData, selectedState, county, impact);
+};
+
+// County Event Handler - Load County Level Plot
+function countyChanged(county) {
+
+    var state = d3.select("#state-select").property("value");
+    var impact = d3.select("#impact-select").property("value");
+
+    createCountyLevelPlot(tableData, state, county, impact);
+};
+
+// Impact Event Handler - Load State/County Level Plots
+function impactChanged(impact) {
+
+    var state = d3.select("#state-select").property("value");
+    var county = d3.select("#county-select").property("value");
+
+    createStateLevelPlot(tableData, state, impact);
+    createCountyLevelPlot(tableData, state, county, impact);
+};
+
+/***************************************************
+ON PAGE LOAD
+****************************************************/
+// fetch data, load county dropdown & create plots
+d3.json('/fetchdata').then(data => {
+    tableData = data;
+
+    var state = d3.select("#state-select").property("value");
+
+    // load county dropdown
+    loadCountyDropDown(state);
+
+    var county = d3.select("#county-select").property("value");
+    var impact = d3.select("#impact-select").property("value");
+
+    createStateLevelPlot(tableData, state, impact);
+    createCountyLevelPlot(tableData, state, county, impact);
+});
