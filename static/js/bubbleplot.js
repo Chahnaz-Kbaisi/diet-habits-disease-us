9251a6c06f989612d9a7eb304352464f6787574e
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-31 18:21:41 -0500

    Changes

--- static/js/bubbleplot.js
+++ static/js/bubbleplot.js
@@ -1,1 +1,9 @@
-// Bubble Plot
+// Bubble Chart Plot 
+
+// Change page title
+d3.select("#page-title").text("Interactive Visualizations");
+
+// Array variable to hold table data 
+var tableData = []
+
+
