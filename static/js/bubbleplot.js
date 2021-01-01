61afc6e59918e439a026cd05462f58b30b828e6e
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2021-01-01 11:08:59 -0500

    State level function added

--- static/js/bubbleplot.js
+++ static/js/bubbleplot.js
@@ -37,9 +37,37 @@ function createsCountyLevelBubblePlot(data, state, county, impact) {
     };
 
 
-// County Level Bubble Plot:
+    // County Level Bubble Plot:
 
-// Function creates State Level Bubble Plot
+    // Function creates State Level Bubble Plot
+    function createStateLevelBubblePlot(data, state, impact) {
+        var yearFilter = data;
+        if (impact == "% Limited Access to Healthy Foods") {
+            yearFilter = yearFilter.filter(row => row["Year"] >= "2013");
+        } else if (impact == "High School Graduation Rate") {
+            yearFilter = yearFilter.filter(row => row["Year"] >= "2013");
+        } else if (impact == "Food Environment Index") {
+            yearFilter = yearFilter.filter(row => row["Year"] >= "2014");
+        } else if (impact == "% With Access to Exercise Opportunities") {
+            yearFilter = yearFilter.filter(row => row["Year"] >= "2014");
+        } else if (impact == "Income Ratio") {
+            yearFilter = yearFilter.filter(row => row["Year"] >= "2015");
+        } else {
+            yearFilter = yearFilter.filter(row => row["Year"] != "2010");
+        }
+
+        var stateFilter = yearFilter.filter(row => row["State"] === state);
+        var countyFilter = stateFilter.filter(row => row["County"] === "");
+
+        var yearArray = countyFilter.map(row => row["Year"]);
+        var impactArray = countyFilter.map(row => row[impact]);
+        var obesityArray = countyFilter.map(row => row["% Adults with Obesity"]);
+        var diabetesArray = countyFilter.map(row => row["% Adults with Diabetes"]);
+
+        if (impact == "Median Household Income") {
+            impactArray = impactArray.map(impactVal => impactVal / 1000)
+            var impact = impact + " (in Thousands)"
+        };
 
 // State Level Bubble Chart Plot
 
