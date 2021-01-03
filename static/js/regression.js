7c04ddd48c76022bbe82d4efaf24ecb9098cc34e
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2021-01-03 16:57:02 -0500

    Function that generates the regression plot and displays the plot in the page

--- static/js/regression.js
+++ static/js/regression.js
@@ -25,3 +25,22 @@ function enableFields() {
     $("#impact-select").attr("disabled", null).attr('style', null);
     $("#year-select").attr("disabled", null).attr('style', null);
 }
+
+// generates the regression plot and displays the plot in the page
+function createRegressionPlot(disease, impact, year) {
+    // disable fields and display message to user
+    disableFields();
+
+    // remove the previous plot (if any)
+    $("#regressionPlot").empty();
+
+    // generate regression plot
+    var url = "/embedRegression/" + year + "/" + impact + "/" + disease;
+    $.get(url, function(data) {
+        // display the generated plot in the page
+        $("#regressionPlot").append(data);
+
+        // enable the fields and remove the message
+        enableFields();
+    });
+}
