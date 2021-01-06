934e29fd1699ca3c839592e74eb517fd9c2928cc
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2021-01-05 22:13:14 -0500

    Functions to disable and enable the fields

--- static/js/leafletmap.js
+++ static/js/leafletmap.js
@@ -18,3 +18,29 @@ var API_KEY;
 d3.json('/getapikey').then(key => {
     API_KEY = key;
 });
+
+/***************************************************
+USER DEFINED FUNCTIONS
+****************************************************/
+
+// Disables the fields and displays a message to the user
+function disableFields() {
+    // disable the fields
+    $("#impact-select").attr("disabled", "disabled").attr('style', 'background: darkgray');
+    $("#year-select").attr("disabled", "disabled").attr('style', 'background: darkgray');
+
+    // display a message to the user
+    $(".message").attr('style', 'margin-top: 100px; margin-bottom: 100px;');
+    $("#loading").attr('style', 'text-align:center').text('Loading Map - Please wait...');
+}
+
+// Enables the fields and removes the message
+function enableFields() {
+    // remove the message
+    $(".message").attr('style', null);
+    $("#loading").attr('style', null).text("");
+
+    // enable the fields
+    $("#impact-select").attr("disabled", null).attr('style', null);
+    $("#year-select").attr("disabled", null).attr('style', null);
+}
