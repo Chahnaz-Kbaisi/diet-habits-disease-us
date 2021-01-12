18db805bb764656c9ea961401ad1393be703b357
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 12:39:29 -0500

    Removed data filtering from JS as it will be performed in Server side

--- static/js/barplot.js
+++ static/js/barplot.js
@@ -56,19 +56,6 @@ function createCountyLevelPlot(data, state, county, impact) {
 
 // function creates State Level Bar Plot
 function createStateLevelPlot(data, state, impact) {
-    var yearFilter = data;
-    if (impact == "% Limited Access to Healthy Foods" || impact == "High School Graduation Rate") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2013");
-    } else if (impact == "Food Environment Index" || impact == "% With Access to Exercise Opportunities") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2014");
-    } else if (impact == "Income Ratio") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2015");
-    } else {
-        yearFilter = yearFilter.filter(row => row["Year"] != "2010");
-    }
-
-    var stateFilter = yearFilter.filter(row => row["State"] === state);
-    var countyFilter = stateFilter.filter(row => row["County"] === "");
 
     var yearArray = countyFilter.map(row => row["Year"]);
     var impactArray = countyFilter.map(row => row[impact]);
