7f431625ec39ff63c47208f30ca77dd760458184
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 12:48:48 -0500

    Removed old api call for fetching all data from server side

--- static/js/barplot.js
+++ static/js/barplot.js
@@ -182,20 +182,13 @@ ON PAGE LOAD
 d3.select("#state-select").attr("disabled", "disabled").style("background", "gray");
 d3.select("#impact-select").attr("disabled", "disabled").style("background", "gray");
 
-// fetch data, load county dropdown & create plots
-d3.json('/fetchdata').then(data => {
-    tableData = data;
-    d3.select("#state-select").attr("disabled", null).style("background", null);
-    d3.select("#impact-select").attr("disabled", null).style("background", null);
+var state = d3.select("#state-select").property("value");
 
-    var state = d3.select("#state-select").property("value");
+// load county dropdown
+loadCountyDropDown(state);
 
-    // load county dropdown
-    loadCountyDropDown(state);
+var county = d3.select("#county-select").property("value");
+var impact = d3.select("#impact-select").property("value");
 
-    var county = d3.select("#county-select").property("value");
-    var impact = d3.select("#impact-select").property("value");
-
-    createStateLevelPlot(tableData, state, impact);
-    createCountyLevelPlot(tableData, state, county, impact);
-});
+createStateLevelPlot(tableData, state, impact);
+createCountyLevelPlot(tableData, state, county, impact);
