20ddf8d7f095ef128737b7a8bd034809b930b64f
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 12:47:07 -0500

    Logic to retrieve the plot data from Server side and create plots in County Event Handler

--- static/js/barplot.js
+++ static/js/barplot.js
@@ -153,7 +153,11 @@ function countyChanged(county) {
     var state = d3.select("#state-select").property("value");
     var impact = d3.select("#impact-select").property("value");
 
-    createCountyLevelPlot(tableData, state, county, impact);
+    // fetch data & create plots
+    d3.json(`/fetchPlotStateCountyData/${state}/${county}/${impact}`).then(data => {
+        var countyData = data[0]["CountyPlotData"];
+        createCountyLevelPlot(countyData, state, county, impact);
+    });
 };
 
 // Impact Event Handler - Load State/County Level Plots
