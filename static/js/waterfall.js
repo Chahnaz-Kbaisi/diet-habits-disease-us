0b3f4d6d7f80f13206b79f4bfe704fdf8a33012c
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 13:37:43 -0500

    Removed global variable that will not be used anymore

--- static/js/waterfall.js
+++ static/js/waterfall.js
@@ -1,9 +1,6 @@
 // Change page title
 d3.select("#page-title").text("Interactive Visualizations");
 
-// Array variable to hold table data 
-var tableData = []
-
 /***************************************************
 USER DEFINED FUNCTIONS
 ****************************************************/
