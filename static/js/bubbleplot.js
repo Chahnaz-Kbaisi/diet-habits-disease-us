370c55b915602717445446d14d094c3d8e1a1bfa
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 14:33:25 -0500

    Removed filtering from JS as it is being done in server side

--- static/js/bubbleplot.js
+++ static/js/bubbleplot.js
@@ -6,20 +6,6 @@ d3.select("#page-title").text("Interactive Visualizations");
 // Followed the same procedure to set the filters as in the line plot
 // Function creates County Level Bubble Plot
 function createCountyLevelBubblePlot(data, state, county, impact) {
-    var yearFilter = data;
-    if (impact == "% Limited Access to Healthy Foods" || impact == "High School Graduation Rate") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2013");
-    } else if (impact == "Food Environment Index" || impact == "% With Access to Exercise Opportunities") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2014");
-    } else if (impact == "Income Ratio") {
-        yearFilter = yearFilter.filter(row => row["Year"] >= "2015");
-    } else {
-        yearFilter = yearFilter.filter(row => row["Year"] != "2010");
-    }
-
-    var stateFilter = yearFilter.filter(row => row["State"] === state);
-    var countyFilter = stateFilter.filter(row => row["County"] == county);
-
     var yearArray = countyFilter.map(row => row["Year"]);
     var impactArray = countyFilter.map(row => row[impact]);
     var obesityArray = countyFilter.map(row => row["% Adults with Obesity"]);
