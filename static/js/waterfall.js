8b373cf8b04e1c5a01d1fb5165e887e631f124dc
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 13:50:32 -0500

    Removed variables that are not being used anywhere

--- static/js/waterfall.js
+++ static/js/waterfall.js
@@ -152,8 +152,6 @@ function impactChanged(impact) {
 ON PAGE LOAD
 ****************************************************/
 
-var prevStateBkgnd = d3.select("#state-select").style("background");
-var prevImpactBkgnd = d3.select("#impact-select").style("background");
 d3.select("#state-select").attr("disabled", "disabled").style("background", "gray");
 d3.select("#impact-select").attr("disabled", "disabled").style("background", "gray");
 console.log("After disabling the state and impact");
