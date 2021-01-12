0553c08279fcb7c3bd81927fc8dfd34fc27ab121
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 12:42:27 -0500

    Removed the var declaration since the variable already exists

--- static/js/barplot.js
+++ static/js/barplot.js
@@ -58,7 +58,7 @@ function createCountyLevelPlot(data, state, county, impact) {
 function createStateLevelPlot(data, state, impact) {
 
     // sort by year
-    var data = data.sort((a, b) => b["Year"] - a["Year"]);
+    data = data.sort((a, b) => b["Year"] - a["Year"]);
 
     var yearArray = data.map(row => row["Year"]);
     var impactArray = data.map(row => row[impact]);
