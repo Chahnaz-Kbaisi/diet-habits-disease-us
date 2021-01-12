e83f487e0638f9759f57ee79cc4f0d09c4931c88
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 11:35:11 -0500

    Will be fetching county data from server side Flask API so removing the logic in JS for data filtering/sorting

--- static/js/lineplot.js
+++ static/js/lineplot.js
@@ -94,12 +94,6 @@ function createStateLevelPlot(data, state, impact) {
 
 // function loads County Dropdown options
 function loadCountyDropDown(selectedState) {
-    var stateFilteredData = tableData.filter(row => row.State === selectedState);
-    var countiesList = stateFilteredData.map(row => row.County);
-    var uniqueCounties = d3.set(countiesList).values();
-
-    // sort the counties in ascending
-    uniqueCounties.sort(d3.ascending)
 
     // Load the County dropdown
     var countyDropDown = d3.select("#county-select");
