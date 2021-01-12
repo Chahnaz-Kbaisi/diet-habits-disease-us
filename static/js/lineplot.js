a22c43c32bfa1c4eb740f00076a72e25c53a670f
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-01-12 11:57:27 -0500

    Removed unwanted code

--- static/js/lineplot.js
+++ static/js/lineplot.js
@@ -180,10 +180,3 @@ var impact = "% Limited Access to Healthy Foods";
 
 // load county dropdown
 loadCountyDropDown(state);
-
-var county = d3.select("#county-select").property("value");
-var impact = d3.select("#impact-select").property("value");
-
-createStateLevelPlot(tableData, state, impact);
-createCountyLevelPlot(tableData, state, county, impact);
-});
